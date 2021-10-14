eb7ba3993d13f8c2aa4b336ca8b0b3a27f570b76
PaoloPascucci <pascuccip9@gmail.com>
2021-10-14 15:08:37 +0200

    Add lista email

---
+++ assets/main.js
@@ -0,0 +1,22 @@
+/*Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+*/
+/*
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+const UserEmail = prompt('Mi serve la tua Email, grazie')
+console.log(UserEmail);
+//- prompt (email)
+const Lista = ["paolopascucci@gmail.com", "pippo@hotmal.com", "giovanni@belda.net", "pablo.valerio@silva.it", "john@lennon.gov"]
+console.log(Lista[0]);
+console.log(Lista[1]);
+console.log(Lista[2]);
+console.log(Lista[3]);
+console.log(Lista[4]);
+//- array con email
+//controllo email
+
+
+//- messaggio sull'esito del controllo
